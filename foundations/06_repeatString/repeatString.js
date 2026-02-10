365d44068e51bea8f1e74f19e0291aa678b1666e
Taaccii <tom1991@hotmail.it>
2026-02-10 11:18:32 +0100

    add completed foundations exericses 06-07-08

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+  let result = ''
 
+  for (let i = 0; i < num; i++) {
+    result += str;
+  }
+
+    if (num < 0) {
+      return 'ERROR';
+    } else {
+      return result;
+    }
+  
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
+  
+  const result = arr.filter(item => !arg.includes(item));
+  return result;
 };
+
+console.log(removeFromArray([1, 2, 3], "1", 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
