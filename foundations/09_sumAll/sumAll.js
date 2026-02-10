3fabdb343752bc20cd80254fe5ef93b72686405d
Taaccii <tom1991@hotmail.it>
2026-02-10 13:00:27 +0100

    add completed foundations exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  let arr = [];
+
+  if (typeof start !== 'number' || typeof end !== 'number') {
+    return 'ERROR'
+  }
+
+  if (!Number.isInteger(start) || !Number.isInteger(end)) {
+    return 'ERROR'
+
+  } else if (start < 0 || end < 0) {
+    return 'ERROR';
+
+  } else if (start <= end ) {
+
+    for (let i = start; i <= end; i++) {
+      arr.push(i);
+    }
+  } else {
+
+    for (let i = start; i >= end; i--) {
+      arr.push(i);
+    }
+  }
+  return arr.reduce((sum, current) => sum + current, 0);
+
 
 };
+
+console.log(sumAll(2.5, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
