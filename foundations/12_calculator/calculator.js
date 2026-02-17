9eda762bf3510378d856911c2ee416516d360f8c
Taaccii <tom1991@hotmail.it>
2026-02-17 17:17:02 +0100

    add completed foundations 12 exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+
+  let result = 1;
+
+	for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+
+  return result;
 };
+
+
+console.log(factorial(10));
 
 // Do not edit below this line
 module.exports = {
