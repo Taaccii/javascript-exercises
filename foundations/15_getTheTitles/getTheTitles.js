ba34ff1a29aa9fa8f2aa26af16de47f5518193de
Taaccii <tom1991@hotmail.it>
2026-02-18 11:05:24 +0100

    add completed exercise 15 from foundations

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  let titleFiltered = array.map(titles => `${titles.title}`);
+  return titleFiltered;
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
