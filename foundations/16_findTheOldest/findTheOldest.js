172ed96014da714af23df6f0be0f1159cd7429c5
Taaccii <tom1991@hotmail.it>
2026-02-18 11:59:28 +0100

    add completed exercise 16 from foundations

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,39 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+  if(!death) return death = new Date().getFullYear() - birth;
+  return death - birth;
+};
+
+
+const findTheOldest = function(array) {
+  
+  return array.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+    return currentAge > oldestAge ? currentPerson : oldest;
+  });
 
 };
+
+
+    const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 2018,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+
+console.log(findTheOldest(people).name);
 
 // Do not edit below this line
 module.exports = findTheOldest;
